7ffba6a4caa2e00244c99cc32f9bd268b2723832
Chris <boyd111c@gmail.com>
2026-08-01 16:26:30 -0600

    Fix XP Rep bar max level bug

--- libs/oUF_Plugins/oUF_XPBar/oUF_XPBar.lua
+++ libs/oUF_Plugins/oUF_XPBar/oUF_XPBar.lua
@@ -102,7 +102,7 @@ local function Update(self, event)
 	maxRep = math.abs(minRep - maxRep)
 
 	if(element.xpBar) then
-		if( UnitLevel(unit) == MAX_PLAYER_LEVEL or (UnitIsUnit(unit, "pet") and UnitLevel(unit) == UnitLevel("player")) ) then
+		if( UnitLevel(unit) == GetMaxPlayerLevel() or (UnitIsUnit(unit, "pet") and UnitLevel(unit) == UnitLevel("player")) ) then
 			element.xpBar:Hide()
 		else
 			element.xpBar:SetMinMaxValues(minXP, maxXP)
